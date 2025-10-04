5c1369a60f92996810e9b2b229c420311c7f404a
Artem Melnyk <melkam464@gmail.com>
2025-10-04 15:13:24 +0200

    fix tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: false },
-  modules: ["@nuxtjs/tailwindcss"],
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: false },
+	css: ["~/assets/css/main.css"],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });

916206c09648dc996e90af9c4d70240db53cf315
Artem Melnyk <melkam464@gmail.com>
2025-10-04 16:00:14 +0200

    upd

---
+++ generate-description.ts
@@ -0,0 +1,6 @@
+// I need to generate description for the image using openai api
+
+import { configDotenv } from "dotenv";
+configDotenv();
+
+const IMAGE_PATH = "./public/images/coffee-break-lemur/1.jpg";
---
+++ shared/quest.ts
@@ -0,0 +1,33 @@
+export const quest = [
+	{
+		description: "Coffee Break Lemur",
+		images: [
+			"/images/coffee-break-lemur/1.jpg",
+			"/images/coffee-break-lemur/2.jpg",
+		],
+	},
+	{
+		description: "Lemur hero with pizza",
+		images: ["/images/pizza-lemur/1.jpg"],
+	},
+	{
+		description: "Restration point with lemur",
+		images: [
+			"/images/registration-lemur/1.jpg",
+			"/images/registration-lemur/2.jpg",
+			"/images/registration-lemur/3.jpg",
+		],
+	},
+	{
+		description: "Main area Hack Yeach 2025",
+		images: [
+			"/images/quest-point/1.jpg",
+			"/images/quest-point/2.jpg",
+			"/images/quest-point/3.jpg",
+		],
+	},
+	{
+		description: "RedBull stage with tetris game",
+		images: ["/images/redbull-stage/1.jpg"],
+	},
+];
